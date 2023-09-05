5c977f6988b858f998143e5946c3969a2777fe08
Alex <alex97av@gmail.com>
2023-09-05 10:39:48 +0200

    fine

--- src/CollegePerson.java
+++ src/CollegePerson.java
@@ -10,6 +10,7 @@ public class CollegePerson {
     }
 
     public void goToCollege() {
-        System.out.println(name + " " + surname + "\nCollege ID: " + collegeId);
+        System.out.println(name + " " + surname +
+                "\nCollege ID: " + collegeId);
     }
 }
